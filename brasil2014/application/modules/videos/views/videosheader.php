b637139dd50511cf324e23badc4a94d7843e8068
Byron <byronherrera@hotmail.com>
2014-05-27 15:29:21 -0500

    vide

---
+++ brasil2014/application/modules/videos/views/videosheader.php
@@ -0,0 +1,5 @@
+<!--        Contenidos principales-->
+<!-- Tab panes -->
+<div class="tab-content contenedor-videos">
+    <iframe width="100%"  height="384px" src="//www.youtube.com/embed/Zyh5yDriaAk" frameborder="0" allowfullscreen></iframe>
+</div>
