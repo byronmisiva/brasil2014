7359041c97dbb2f167f293e8e93ce857d0ba032e
Byron <byronherrera@hotmail.com>
2014-05-28 12:34:14 -0500

    subir2

---
+++ brasil2014/application/modules/contenido/views/bannercontenido.php
@@ -0,0 +1,11 @@
+<!-- Banner    -->
+<div class="col-md-12 margen0 separador">
+    <!-- 650X90_MOVISTAR_MUNDIAL -->
+    <div id='div-gpt-ad-1401289674699-1' style='width:650px; height:90px;'>
+        <script type='text/javascript'>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-1401289674699-1'); });
+        </script>
+    </div>
+</div>
+<div class="clearfix"></div>
+<!-- Fin Banner   -->
