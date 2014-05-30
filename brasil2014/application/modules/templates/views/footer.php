eb8afb901ca080447dc38ea6f76621add9b057c8
Byron <byronherrera@hotmail.com>
2014-05-30 14:40:32 -0500

    car

--- brasil2014/application/modules/templates/views/footer.php
+++ brasil2014/application/modules/templates/views/footer.php
@@ -4,13 +4,15 @@
 
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 
-<script src="<?php echo base_url('assets/js/jquery.countdown.min.js') ?>"></script>
-<script src="<?php echo base_url('assets/js/jquery.countdown-es.js') ?>"></script>
 <script src="<?php echo base_url('/assets/js/bootstrap.min.js') ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/carouFredSel/jquery.carouFredSel-6.2.1-packed.js') ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('/assets/js/colorbox/jquery.colorbox.min.js') ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('/assets/js/scrool/animatescroll.min.js') ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('/assets/js/brasil2014.min.js') ?>"></script>
+
+<script src="<?php echo base_url('assets/js/jquery.countdown.js') ?>"></script>
+<script src="<?php echo base_url('assets/js/jquery.countdown-es.js') ?>"></script>
+
 <!-- optionally include helper plugins -->
 
 <!--<script type="text/javascript"
